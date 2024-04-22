ff31f6a8e201a372d188459efbe9c593c51ac2f6
thuyA3T3 <thuydao13042002@gmail.com>
2024-04-22 22:12:40 +0700

    [FEATURE] login anh register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,7 @@ namespace App\Providers;
 
 use App\Interfaces\Email\EmailServiceInterface;
 use App\Interfaces\User\UserRepositoryInterface;
-use App\Interfaces\AuthReponsitoryInterface;
 use App\Repositories\User\UserRepository;
-use App\Repositories\Auth\AuthRepository;
 use App\Services\Email\EmailService;
 use Illuminate\Support\ServiceProvider;
 
@@ -19,7 +17,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(AuthReponsitoryInterface::class, AuthRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(EmailServiceInterface::class, EmailService::class);
     }
